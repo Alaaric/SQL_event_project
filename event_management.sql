31dd1f9008568f2ff5efeb8e3e624a92bfa565c2
Alaric HENROT <alaric.henrot@gmail.com>
2025-11-19 09:39:01 +0100

    add mysql user

--- event_management.sql
+++ event_management.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE event_management;
+USE event_management;
+
+CREATE USER 'app'@'localhost' IDENTIFIED BY 'unMotDePasse';
+
+GRANT SELECT ON event_management.* TO 'app'@'localhost';
+GRANT EXECUTE ON event_management.* TO 'app'@'localhost';
+FLUSH PRIVILEGES;
